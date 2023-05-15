1f89373c721341f281439fcea231bb74d3ba7483
beom1220 <117291257+beom1220@users.noreply.github.com>
2023-05-15 21:14:11 +0900

    주간 수면 그래프 추가
    주간 수면 그래프 추가

--- app/src/main/java/com/example/narcolepsyproject/SleepChartActivity.java
+++ app/src/main/java/com/example/narcolepsyproject/SleepChartActivity.java
@@ -6,17 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class SleepChartActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    BarChart sleepChart;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -60,5 +73,49 @@ public class SleepChartActivity extends AppCompatActivity {
                 }
             }
         });
+        BarChart sleepChart = findViewById(R.id.sleepChart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 7));   // 일요일
+        entries.add(new BarEntry(1, 6));   // 월요일
+        entries.add(new BarEntry(2, 5));   // 화요일
+        entries.add(new BarEntry(3, 8));   // 수요일
+        entries.add(new BarEntry(4, 7));   // 목요일
+        entries.add(new BarEntry(5, 6));   // 금요일
+        entries.add(new BarEntry(6, 5));   // 토요일
+
+        BarDataSet dataSet = new BarDataSet(entries, "Sleep Data");
+        dataSet.setColor(Color.BLUE);
+
+        ArrayList<String> labels = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+
+        for (int i = 0; i < 7; i++) {
+            labels.add(getDayOfWeek(calendar.get(Calendar.DAY_OF_WEEK)));
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+        }
+
+        BarData data = new BarData(dataSet);
+        sleepChart.setData(data);
+
+        XAxis xAxis = sleepChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+
+        YAxis yAxis = sleepChart.getAxisLeft();
+        yAxis.setAxisMinimum(0f);
+
+        sleepChart.getAxisRight().setEnabled(false);
+        Description description = new Description();
+        description.setEnabled(false);
+        sleepChart.setDescription(description);
+        sleepChart.invalidate();
+
+    }
+    private String getDayOfWeek(int dayOfWeek) {
+        String[] daysOfWeek = {"일", "월", "화", "수", "목", "금", "토"};
+        return daysOfWeek[dayOfWeek - 1];
     }
 }
