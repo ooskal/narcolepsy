1e6bdf62a7c318d8c181ff229a0d0dfb74ba1838
Soo Yeon Kim <104661679+ooskal@users.noreply.github.com>
2023-06-05 21:07:02 +0900

    django api/mysql 1.0
    api 서버

--- app/src/main/java/com/example/narcolepsyproject/NokActivity.java
+++ app/src/main/java/com/example/narcolepsyproject/NokActivity.java
@@ -14,40 +14,85 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.example.narcolepsyproject.db.RoomDB;
-import com.example.narcolepsyproject.db.contact.ContactAdapter;
-import com.example.narcolepsyproject.db.contact.ContactData;
+import com.example.narcolepsyproject.db.ContactPost;
+import com.example.narcolepsyproject.db.JsonPlaceHolderApi;
+//import com.example.narcolepsyproject.db.contact.ContactAdapter;
+//import com.example.narcolepsyproject.db.contact.ContactData;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Call;
+
 public class NokActivity extends AppCompatActivity {
+    TextView textView; // retrofit
 
     RecyclerView recyclerView;
     Button btnAdd;
     EditText editNameText,editPhoneNumberText;
 
     //데이터베이스
-    List<ContactData> dataList = new ArrayList<>();
-    RoomDB database;
-    ContactAdapter adapter;
+//    List<ContactData> dataList = new ArrayList<>();
+//    RoomDB database;
+//    ContactAdapter adapter;
 
 
     BottomNavigationView bottomNavigationView;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
 
-    ArrayList<HashMap<String, String>> Data = new ArrayList<HashMap<String, String>>();
-    HashMap<String, String> InputData1 = new HashMap<>();
-    HashMap<String, String> InputData2 = new HashMap<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nok);
+
+        // retrofit2
+        textView = findViewById(R.id.textView);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.64.101:8000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        Call<List<ContactPost>> call = jsonPlaceHolderApi.getContactPosts();
+
+        call.enqueue((new Callback<List<ContactPost>>() {
+            @Override
+            public void onResponse(Call<List<ContactPost>> call1, Response<List<ContactPost>>response) {
+
+                if(!response.isSuccessful()) {
+                    textView.setText("Code:" + response.code());
+                    return;
+                }
+
+                List<ContactPost> contactPosts = response.body();
+
+                for(ContactPost contactPost : contactPosts) {
+                    String content ="";
+                    content +=  "이름: "+ contactPost.getName() +"\n";
+                    content +=  "번호: "+ contactPost.getPhoneNumber() +"\n";
+
+                    textView.append(content);
+                }
+
+            }
+            @Override
+            public void onFailure(Call<List<ContactPost>>call, Throwable t){
+                textView.setText(t.getMessage());
+            }
+        }));
+
+        //
 
         bottomNavigationView = findViewById(R.id.bottomNav);
 
@@ -92,15 +137,15 @@ public class NokActivity extends AppCompatActivity {
         editPhoneNumberText= findViewById(R.id.editPhoneNumberText);
         recyclerView = findViewById(R.id.recycler_view); //activity_nok - recycler_view
 
-        database = RoomDB.getInstance(this);
+//        database = RoomDB.getInstance(this);
+//
+//        dataList = database.mainDao().getAll();
+//
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        adapter = new ContactAdapter(NokActivity.this, dataList);
 
-        dataList = database.mainDao().getAll();
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        adapter = new ContactAdapter(NokActivity.this, dataList);
-
-        recyclerView.setAdapter(adapter);
+//        recyclerView.setAdapter(adapter);
 
         btnAdd.setOnClickListener(new View.OnClickListener()
         {
@@ -109,53 +154,65 @@ public class NokActivity extends AppCompatActivity {
             {
                 String name = editNameText.getText().toString().trim();
                 String phoneNumber = editPhoneNumberText.getText().toString().trim();
-                if (!name.equals("") )
-                {
-                    ContactData data = new ContactData();
-                    data.setName(name);
-                    data.setPhoneNumber(phoneNumber);
-                    database.mainDao().insert(data);
+//                if (!name.equals("") )
+//                {
+//                    ContactData data = new ContactData();
+//                    data.setName(name);
+//                    data.setPhoneNumber(phoneNumber);
+//                    database.mainDao().insert(data);
+//
+//                    Log.d("Data", "Inserted: " + data.getName() + ", " + data.getPhoneNumber());
+//
+//
+//                    editNameText.setText("");
+//                    editPhoneNumberText.setText("");
+//
+//                    dataList.clear();
+//                    dataList.addAll(database.mainDao().getAll());
+//                    adapter.notifyDataSetChanged();
+//
+//                }
+                if (!name.equals("")) {
+                    // Retrofit 인스턴스 생성
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("http://192.168.64.101:8000/") // 장고 서버의 기본 URL
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
 
-                    Log.d("Data", "Inserted: " + data.getName() + ", " + data.getPhoneNumber());
+                    // Retrofit 서비스 인터페이스 생성
+                    JsonPlaceHolderApi apiService = retrofit.create(JsonPlaceHolderApi.class);
 
+                    // 이름과 전화번호를 RequestBody 형태로 변환
+                    RequestBody nameRequestBody = RequestBody.create(MediaType.parse("text/plain"), name);
+                    RequestBody phoneNumberRequestBody = RequestBody.create(MediaType.parse("text/plain"), phoneNumber);
+
+                    // API 호출 및 데이터 전송
+                    Call<List<ContactPost>> call = apiService.postContact(nameRequestBody, phoneNumberRequestBody);
+                    call.enqueue(new Callback<List<ContactPost>>() {
+                        @Override
+                        public void onResponse(Call<List<ContactPost>> call, Response<List<ContactPost>> response) {
+                            if (response.isSuccessful()) {
+                                Log.d("Data", "Data saved successfully");
+                            } else {
+                                Log.e("Data", "Failed to save data");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<ContactPost>> call, Throwable t) {
+                            Log.e("Data", "Error while saving data: " + t.getMessage());
+                        }
+                    });
 
                     editNameText.setText("");
                     editPhoneNumberText.setText("");
-
-                    dataList.clear();
-                    dataList.addAll(database.mainDao().getAll());
-                    adapter.notifyDataSetChanged();
-
+//                    dataList.clear();
+//                    dataList.addAll(database.mainDao().getAll());
+//                    adapter.notifyDataSetChanged();
                 }
+
+
             }
         });
-
-
-
-
-
-//        String[] nok = {"엄마", "아빠"};
-
-//        ListView lv = (ListView) findViewById(R.id.phoneNumList);
-//
-//        InputData1.put("nok", "엄마");
-//        InputData1.put("num", "010-0000-0000");
-//        Data.add(InputData1);
-//
-//        InputData2.put("nok", "아빠");
-//        InputData2.put("num", "010-1111-1111");
-//        Data.add(InputData2);
-
-//        SimpleAdapter simpleAdapter = new SimpleAdapter(this, Data, android.R.layout.simple_list_item_2, new String[]{"nok", "num"}, new int[]{android.R.id.text1, android.R.id.text2});
-//        lv.setAdapter(simpleAdapter);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nok);
-//        lv.setAdapter(adapter);
-//
-//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Toast.makeText(getApplicationContext(), nok[i], Toast.LENGTH_SHORT).show();
-//            }
-//        });
     }
 }
